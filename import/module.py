6774991803cbae822cf7564ccc4994dc4b16c233
shreekant-lohagale <shreekantlohagale@gmail.com>
2025-11-14 16:00:43 +0530

    use dt instead of dateime from now on

--- import/module.py
+++ import/module.py
@@ -7,3 +7,11 @@ print("Today's date:", today)
 print("Year:", today.year)
 print("Month:", today.month)
 print("Day:", today.day)
+
+start_time = datetime.datetime.now()
+start_time_str = start_time.strftime("%Y-%m-%d %H:%M:%S")
+print("Start Time:", start_time_str)
+
+import datetime as dt
+start_time = dt.datetime.now()
+start_time.replace(month = 12, day = 25)
