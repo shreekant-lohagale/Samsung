3c287a51f568c01a655239b6ff114ded759cd282
shreekant-lohagale <shreekantlohagale@gmail.com>
2025-11-14 15:55:03 +0530

    Get today's date

--- import/module.py
+++ import/module.py
@@ -1,2 +1,9 @@
 import datetime
 print(datetime.datetime.now())
+
+
+today = datetime.date.today()
+print("Today's date:", today)
+print("Year:", today.year)
+print("Month:", today.month)
+print("Day:", today.day)
