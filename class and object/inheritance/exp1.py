a0df267d01aa5b855e0feb010e54f800d4119ae3
shreekant-lohagale <shreekantlohagale@gmail.com>
2026-01-02 11:02:36 +0530

    Add Bike and Boat classes with methods for movement and transportation function

--- class and object/inheritance/exp1.py
+++ class and object/inheritance/exp1.py
@@ -8,3 +8,17 @@ class Cat:
 
 for animal in [Dog(), Cat()]:
     print(animal.spark())
+
+class Bike():
+    def move(self):
+        print("Bike is moving")
+
+class Boat():
+    def move(self):
+        print("Boat is moving")
+
+def trasportation(vehicle):
+    vehicle.move()
+
+trasportation(Bike())
+trasportation(Boat())
