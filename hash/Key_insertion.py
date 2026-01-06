387098ff4a12d40bc42abb13dd5fab563d837ff6
shreekant-lohagale <shreekantlohagale@gmail.com>
2026-01-06 09:44:05 +0530

    Add hash table implementation with key insertion and display of final table

---
+++ hash/Key_insertion.py
@@ -0,0 +1,22 @@
+#Search Key 
+keys = [24, 18, 13, 56, 45, 72, 90, 11, 67, 39]
+
+#size of hash table 
+n = 10
+
+#create empty hash table
+hash_table = [None] * n
+
+#hash function
+def hash_fuction(k):
+    return k % n
+
+#insert keys into hash table 
+for key in keys:
+    index = hash_fuction(key)
+    hash_table[index] = key
+    print(f"key: {key} stored at index: {index}")
+
+print("final hash table")
+for i in range(n):
+    print(f"Index {i}: {hash_table[i]}")
